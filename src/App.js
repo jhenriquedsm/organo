1e8b2395cd69151376a819af8ff4931caefd5145
jhenriquedsm <jhsilvamata@gmail.com>
2024-08-29 18:33:02 -0300

    creating team component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/Team';
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form registeredCollaborator={collaborator => newCollaboratorAdd(collaborator)}/>
+      <Team name='Governança de TI'/>
+      <Team name='Inovação e Gestão'/>
+      <Team name='Parcerias'/>
     </div>
   );
 }
