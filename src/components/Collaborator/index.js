3afa43b9e52ed78c72eeae9bbe7998dd2dd890a7
jhenriquedsm <jhsilvamata@gmail.com>
2024-08-30 13:12:05 -0300

    collaborator component

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,18 @@
+import './Collaborator.css'
+
+const Collaborator = () => {
+    return (
+    <div className='collaborator'>
+        <div className='cabecalho'>
+            <img src='https://github.com/jhenriquedsm.png' alt='José Henrique' />
+        </div>
+        <div className='rodape'>
+            <h4>José Henrique</h4>
+            <h5>Estagiário</h5>
+        </div>
+    </div>
+
+    )
+}
+
+export default Collaborator
